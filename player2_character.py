e0a77df9b860c96c8e648bc56e508151d1c6abc3
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-07 19:05:55 +0900

    player2 두 번째 캐릭터 Idle 상태와 Run(좌,우 움직임) 구현

--- player2_character.py
+++ player2_character.py
@@ -119,3 +119,99 @@ class Al:
 
     def draw(self):
         self.state_machine.draw()
+
+
+
+# 두 번째 캐릭터 구현
+class Jondahl_Run:
+    def __init__(self, jondahl):
+        self.Jondahl = jondahl
+
+    def enter(self, e):
+        # keyup이 아닌 keydown으로 방향을 결정해야 함
+        if l_down(e):
+            self.Jondahl.dir = self.Jondahl.face_dir = 1
+        elif j_down(e):
+            self.Jondahl.dir = self.Jondahl.face_dir = -1
+
+    def exit(self, e):
+        self.Jondahl.frameX = 0
+        pass
+
+    def do(self):
+        self.Jondahl.frameX = (self.Jondahl.frameX + 1) % 17
+        self.Jondahl.x += self.Jondahl.dir * 10
+
+    def draw(self):
+        # 원본 크기로 중앙 정렬하여 그려 좌우 흔들림을 제거 (스케일링 없음)
+        img = self.Jondahl.images['Jondahl_Run'][int(self.Jondahl.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Jondahl.x
+        if self.Jondahl.face_dir < 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
+
+class Jondahl_Idle:
+    def __init__(self, jondahl):
+        self.Jondahl = jondahl
+
+    def enter(self, e):
+        # 멈출 때 이동 방향(dir)만 0으로 하고, face_dir는 현재 바라보는 방향을 유지
+        self.Jondahl.dir = 0
+    def exit(self, e):
+        self.Jondahl.frameX = 0
+        pass
+
+    def do(self):
+        self.Jondahl.frameX = (self.Jondahl.frameX + 1) % 22
+    def draw(self):
+        # 원본 크기로 중앙 정렬하여 그려 좌우 흔들림을 제거 (스케일링 없음)
+        img = self.Jondahl.images['Jondahl_Idle'][int(self.Jondahl.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Jondahl.x
+        if self.Jondahl.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
+
+class Jondahl:
+    def __init__(self):
+        self.x, self.y = 97, 50
+        self.frameX = 0
+        self.frameY = 0
+        self.face_dir = -1
+        self.dir = -1
+        self.animation_names = ['Jondahl_Idle', 'Jondahl_Run']
+        self.images = {}
+        self.render_size = {}
+        for name in self.animation_names:
+            if name == 'Jondahl_Idle':
+                frames = [load_image("./Jondahl/" + name + " (%d)" % i + ".png") for i in range(1, 23)]
+            elif name == 'Jondahl_Run':
+                frames = [load_image("./Jondahl/" + name + " (%d)" % i + ".png") for i in range(1, 18)]
+            self.images[name] = frames
+            max_w = max(img.w for img in frames)
+            max_h = max(img.h for img in frames)
+            self.render_size[name] = (max_w, max_h)
+
+        self.IDLE = Jondahl_Idle(self)
+        self.RUN = Jondahl_Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE,
+            {
+                self.IDLE: {i_down: self.IDLE, k_down: self.IDLE, j_down : self.RUN, l_down :self.RUN},
+                self.RUN : {l_up : self.IDLE, j_up : self.IDLE, j_down : self.RUN, l_down : self.RUN},
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        # 들어온 외부 키입력 등을 상태 머신에 전달하기 위해서
+        # 튜플화 시킨 후, 전달
+        self.state_machine.handle_state_event(('INPUT', event))
+
+    def draw(self):
+        self.state_machine.draw()
