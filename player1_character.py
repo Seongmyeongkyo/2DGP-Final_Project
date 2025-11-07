1071e378590340b03dc18c195305be13ba13f1ef
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-07 18:48:50 +0900

    player1을 새로 추가한 스프라이트의 Idle 상태와 Run(좌,우) 상태 수정

--- player1_character.py
+++ player1_character.py
@@ -1,5 +1,4 @@
-from pico2d import load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_w, SDLK_s, SDLK_a, SDLK_d
-
+from pico2d import *
 from state_machine import StateMachine
 
 # 이벤트를 체크하는 함수들을 구현
@@ -22,8 +21,8 @@ def d_down(e):
 def d_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_d
 
-
-class Run:
+# 첫 번째 캐릭터 구현
+class Al_Run:
 
     def __init__(self, al):
         self.Al = al
@@ -36,19 +35,23 @@ class Run:
             self.Al.dir = self.Al.face_dir = -1
 
     def exit(self, e):
+        self.Al.frameX = 0
         pass
 
     def do(self):
         self.Al.frameX = (self.Al.frameX + 1) % 8
-        self.Al.frameY = 2
-        self.Al.x += self.Al.dir * 5
+        self.Al.x += self.Al.dir * 10
 
     def draw(self):
-        # 화면 출력 시 바라보는 방향(face_dir)을 사용해서 멈춰있을 때도 올바른 뒤집기 유지
-        flip = 'none' if self.Al.face_dir == -1 else 'h'
-        self.Al.image.clip_composite_draw(self.Al.frameX * 194, self.Al.frameY * 194, 194, 194, 0, flip, self.Al.x, 90, 194, 194)
+        img = self.Al.images['Al_Run'][int(self.Al.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Al.x
+        if self.Al.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
 
-class Idle:
+class Al_Idle:
 
     def __init__(self, al):
         self.Al = al
@@ -57,28 +60,45 @@ class Idle:
         # 멈출 때 이동 방향(dir)만 0으로 하고, face_dir는 현재 바라보는 방향을 유지
         self.Al.dir = 0
     def exit(self, e):
+        self.Al.frameX = 0
         pass
 
     def do(self):
-        self.Al.frameX = (self.Al.frameX + 1) % 8
-        self.Al.frameY = 10
+        self.Al.frameX = (self.Al.frameX + 1) % 25
     def draw(self):
-        flip = 'none' if self.Al.face_dir == -1 else 'h'
-        self.Al.image.clip_composite_draw(self.Al.frameX * 194, self.Al.frameY * 194, 194, 194, 0, flip, self.Al.x, 90, 194, 194)
-
+        # 원본 크기로 중앙 정렬하여 그려 좌우 흔들림을 제거 (스케일링 없음)
+        img = self.Al.images['Al_Idle'][int(self.Al.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Al.x
+        if self.Al.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
 
 
 class Al:
     def __init__(self):
-        self.x, self.y = 97, 90
+        self.x, self.y = 97, 50
         self.frameX = 0
         self.frameY = 0
         self.face_dir = 1
         self.dir = 1
-        self.image = load_image('character_Al.png')
+        self.animation_names = ['Al_Idle', 'Al_Run']
+        self.images = {}
+        # 각 애니메이션별로 최대 프레임 너비/높이를 저장하면 출력 크기를 통일하여 흔들림을 방지할 수 있음
+        self.render_size = {}
+        for name in self.animation_names:
+            if name == 'Al_Idle':
+                frames = [load_image("./AL/" + name + " (%d)" % i + ".png") for i in range(1, 26)]
+            elif name == 'Al_Run':
+                frames = [load_image("./AL/" + name + " (%d)" % i + ".png") for i in range(1, 9)]
+            self.images[name] = frames
+            max_w = max(img.w for img in frames)
+            max_h = max(img.h for img in frames)
+            self.render_size[name] = (max_w, max_h)
 
-        self.IDLE = Idle(self)
-        self.RUN = Run(self)
+        self.IDLE = Al_Idle(self)
+        self.RUN = Al_Run(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
