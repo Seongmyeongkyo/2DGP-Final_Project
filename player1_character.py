5333a60a46af1d842a3f6d623619dfa7647f5a32
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-07 19:22:19 +0900

    player1 네 번째 캐릭터 Idle 상태와 Run(좌,우 움직임) 구현

--- player1_character.py
+++ player1_character.py
@@ -317,3 +317,103 @@ class Zizou_Olympia:
     def draw(self):
         self.state_machine.draw()
 
+
+# 네 번째 캐릭터 구현
+class Franzer_Run:
+
+    def __init__(self, franzer):
+        self.Franzer = franzer
+
+    def enter(self, e):
+        # keyup이 아닌 keydown으로 방향을 결정해야 함
+        if d_down(e):
+            self.Franzer.dir = self.Franzer.face_dir = 1
+        elif a_down(e):
+            self.Franzer.dir = self.Franzer.face_dir = -1
+
+    def exit(self, e):
+        self.Franzer.frameX = 0
+        pass
+
+    def do(self):
+        self.Franzer.frameX = (self.Franzer.frameX + 1) % 6
+        self.Franzer.x += self.Franzer.dir * 10
+
+    def draw(self):
+        img = self.Franzer.images['Franzer_Run'][int(self.Franzer.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Franzer.x
+        if self.Franzer.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
+
+
+class Franzer_Idle:
+
+    def __init__(self, franzer):
+        self.Franzer = franzer
+
+    def enter(self, e):
+        # 멈출 때 이동 방향(dir)만 0으로 하고, face_dir는 현재 바라보는 방향을 유지
+        self.Franzer.dir = 0
+    def exit(self, e):
+        # 이미지 리셋
+        self.Franzer.frameX = 0
+        pass
+
+    def do(self):
+        self.Franzer.frameX = (self.Franzer.frameX + 1) % 3
+    def draw(self):
+        # 원본 크기로 중앙 정렬하여 그려 좌우 흔들림을 제거 (스케일링 없음)
+        img = self.Franzer.images['Franzer_Idle'][int(self.Franzer.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Franzer.x
+        if self.Franzer.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
+
+
+class Franzer:
+    def __init__(self):
+        self.x, self.y = 97, 50
+        self.frameX = 0
+        self.frameY = 0
+        self.face_dir = 1
+        self.dir = 1
+        self.animation_names = ['Franzer_Idle', 'Franzer_Run']
+        self.images = {}
+        # 각 애니메이션별로 최대 프레임 너비/높이를 저장하면 출력 크기를 통일하여 흔들림을 방지할 수 있음
+        self.render_size = {}
+        for name in self.animation_names:
+            if name == 'Franzer_Idle':
+                frames = [load_image("./Franzer/" + name + " (%d)" % i + ".png") for i in range(1, 4)]
+            elif name == 'Franzer_Run':
+                frames = [load_image("./Franzer/" + name + " (%d)" % i + ".png") for i in range(1, 7)]
+            self.images[name] = frames
+            max_w = max(img.w for img in frames)
+            max_h = max(img.h for img in frames)
+            self.render_size[name] = (max_w, max_h)
+
+        self.IDLE = Franzer_Idle(self)
+        self.RUN = Franzer_Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE,
+            {
+                self.IDLE: {w_down: self.IDLE, s_down: self.IDLE, a_down : self.RUN, d_down :self.RUN},
+                self.RUN : {d_up : self.IDLE, a_up : self.IDLE, a_down : self.RUN, d_down : self.RUN},
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        # 들어온 외부 키입력 등을 상태 머신에 전달하기 위해서
+        # 튜플화 시킨 후, 전달
+        self.state_machine.handle_state_event(('INPUT', event))
+
+    def draw(self):
+        self.state_machine.draw()
+
