d737df97683d4a6b26d3e840afca450eabf6df52
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-10 22:10:24 +0900

    player2 네 번째 캐릭터 일반 스킬 모션 구현

--- player2_character.py
+++ player2_character.py
@@ -197,7 +197,6 @@ class Al_Idle:
         else:
             img.draw(draw_x, draw_y)
 
-
 class Al:
     def __init__(self):
         self.x, self.y = 1280-97, 50
@@ -262,7 +261,6 @@ class Al:
         else:
             al_nomal_skill_attack = Al_Nomal_Skill_Attack2(self.x - 82, self.y, self.face_dir)
             game_world.add_object(al_nomal_skill_attack, 1)
-
 
 
 # 두 번째 캐릭터 구현
@@ -700,8 +698,44 @@ class Zizou_Olympia:
             game_world.add_object(olympia_attack, 1)
 
 
-
 # 네 번째 캐릭터 구현
+class Franzer_NomalSkill:
+    def __init__(self, franzer):
+        self.Franzer = franzer
+
+    def enter(self, e):
+        # keyup이 아닌 keydown으로 방향을 결정해야 함
+        if l_down(e):
+            self.Franzer.dir = self.Franzer.face_dir = 1
+        elif j_down(e):
+            self.Franzer.dir = self.Franzer.face_dir = -1
+
+    def exit(self, e):
+        self.Franzer.frameX = 0
+        pass
+
+    def do(self):
+        # 애니메이션 길이와 프레임 타임을 소유자에서 가져와 계산
+        length = self.Franzer.frames_per_animation.get('Franzer_NomalSkill', 1)
+        self.Franzer.TIME_PER_ACTION = 1.0
+        self.Franzer.ACTION_PER_TIME = 1.0 / self.Franzer.TIME_PER_ACTION
+
+        increment = length * self.Franzer.ACTION_PER_TIME * game_framework.frame_time
+        self.Franzer.frameX = (self.Franzer.frameX + increment) % length
+
+        if self.Franzer.frameX >= length - 1:
+            self.Franzer.state_machine.handle_state_event(('TIMEOUT', None))
+
+    def draw(self):
+        # 원본 크기로 중앙 정렬하여 그려 좌우 흔들림을 제거 (스케일링 없음)
+        img = self.Franzer.images['Franzer_NomalSkill'][int(self.Franzer.frameX)]
+        draw_y = 0 + img.h / 2
+        draw_x = self.Franzer.x
+        if self.Franzer.face_dir > 0:
+            img.composite_draw(0, 'h', draw_x, draw_y)
+        else:
+            img.draw(draw_x, draw_y)
+
 class Franzer_Attack:
     def __init__(self, franzer):
         self.Franzer = franzer
@@ -783,7 +817,6 @@ class Franzer_Run:
         else:
             img.draw(draw_x, draw_y)
 
-
 class Franzer_Idle:
 
     def __init__(self, franzer):
@@ -817,7 +850,6 @@ class Franzer_Idle:
         else:
             img.draw(draw_x, draw_y)
 
-
 class Franzer:
     def __init__(self):
         self.x, self.y = 1280 - 97, 50
@@ -825,7 +857,7 @@ class Franzer:
         self.frameY = 0
         self.face_dir = -1
         self.dir = 1
-        self.animation_names = ['Franzer_Idle', 'Franzer_Run', 'Franzer_Attack']
+        self.animation_names = ['Franzer_Idle', 'Franzer_Run', 'Franzer_Attack', 'Franzer_NomalSkill']
         self.images = {}
         # 각 애니메이션별로 최대 프레임 너비/높이를 저장하면 출력 크기를 통일하여 흔들림을 방지할 수 있음
         self.render_size = {}
@@ -840,6 +872,8 @@ class Franzer:
                 frames = [load_image("./Franzer/" + name + " (%d)" % i + ".png") for i in range(1, 7)]
             elif name == 'Franzer_Attack':
                 frames = [load_image("./Franzer/" + name + " (%d)" % i + ".png") for i in range(1, 5)]
+            elif name == 'Franzer_NomalSkill':
+                frames = [load_image("./Franzer/" + name + " (%d)" % i + ".png") for i in range(1, 11)]
             self.images[name] = frames
             max_w = max(img.w for img in frames)
             max_h = max(img.h for img in frames)
@@ -849,12 +883,14 @@ class Franzer:
         self.IDLE = Franzer_Idle(self)
         self.RUN = Franzer_Run(self)
         self.ATTACK = Franzer_Attack(self)
+        self.NOMAL_SKILL = Franzer_NomalSkill(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
-                self.IDLE: {i_down: self.IDLE, k_down: self.IDLE, j_down : self.RUN, l_down :self.RUN, u_down : self.ATTACK},
-                self.RUN : {j_up : self.IDLE, l_up : self.IDLE, j_down : self.RUN, l_down : self.RUN, u_down : self.ATTACK},
-                self.ATTACK : {time_out : self.IDLE, j_down : self.RUN, l_down : self.RUN, u_down : self.ATTACK},
+                self.IDLE: {i_down: self.IDLE, k_down: self.IDLE, j_down : self.RUN, l_down :self.RUN, u_down : self.ATTACK, o_down : self.NOMAL_SKILL},
+                self.RUN : {j_up : self.IDLE, l_up : self.IDLE, j_down : self.RUN, l_down : self.RUN, u_down : self.ATTACK, o_down : self.NOMAL_SKILL},
+                self.ATTACK : {time_out : self.IDLE, j_down : self.RUN, l_down : self.RUN, u_down : self.ATTACK, o_down : self.NOMAL_SKILL},
+                self.NOMAL_SKILL : {time_out : self.IDLE},
             }
         )
 
