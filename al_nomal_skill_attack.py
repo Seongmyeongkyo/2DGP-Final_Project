0085560fc397a8884b47e367bc7c4f875c16e420
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-10 21:32:51 +0900

    player2 첫 번째 캐릭터 일반 스킬 발사체 모듈 구현

--- al_nomal_skill_attack.py
+++ al_nomal_skill_attack.py
@@ -35,3 +35,31 @@ class Al_Nomal_Skill_Attack:
             game_world.remove_object(self)
         if self.x < 0 or self.x > 1280:
             game_world.remove_object(self)
+
+class Al_Nomal_Skill_Attack2:
+    image = None
+
+    def __init__(self, x = 1280 - 97, y = 70, face_dir = -1):
+        if Al_Nomal_Skill_Attack2.image == None:
+            Al_Nomal_Skill_Attack2.image = load_image("./Skill_impact/Al_NomalSkill_Attack.png")
+        self.x, self.y = x, y
+        self.face_dir = face_dir
+        self.right_end_x = x + 300
+        self.left_end_x = x - 300
+
+    def draw(self):
+        if Al_Nomal_Skill_Attack.image and self.face_dir > 0:
+            Al_Nomal_Skill_Attack.image.composite_draw(0, 'h', self.x, self.y, 123, 78)
+        elif Al_Nomal_Skill_Attack.image and self.face_dir < 0:
+            Al_Nomal_Skill_Attack.image.draw(self.x, self.y, 123, 78)
+
+    def update(self):
+        # 위치 업데이트
+        self.x += RUN_SPEED_PPS * self.face_dir * game_framework.frame_time
+        # 화면 밖으로 나가면 제거
+        if self.face_dir < 0 and self.x < self.left_end_x:
+            game_world.remove_object(self)
+        elif self.face_dir > 0 and self.x > self.right_end_x:
+            game_world.remove_object(self)
+        if self.x < 0 or self.x > 1280:
+            game_world.remove_object(self)
