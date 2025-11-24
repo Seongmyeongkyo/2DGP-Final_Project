bc5295a21151867be64a8ed83277ff79f6d2cfa4
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-11-24 18:58:47 +0900

    두 번째 캐릭터 프로필 객체 모듈 구현

---
+++ jondahl_profile.py
@@ -0,0 +1,23 @@
+from pico2d import *
+
+class Jondahl_profile:
+    def __init__(self):
+        self.image = load_image("./UI/Jondahl_profile.png")
+        self.x = 50
+        self.y = 720 - 50
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.composite_draw(0, 'h', self.x, self.y, 100, 100)
+
+class Jondahl_profile2:
+    def __init__(self):
+        self.image = load_image("./UI/Jondahl_profile.png")
+        self.x = 1230
+        self.y = 720 - 50
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y, 100, 100)
