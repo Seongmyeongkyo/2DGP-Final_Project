7ec4d634f4ea1953b7449c7f63b845ec4d521faa
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-12-05 04:02:46 +0900

    logo_mode로 시작되도록 수정

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
 
-import play_mode as start_mode
+import logo_mode as start_mode
 
 open_canvas(1280, 720)
 SDL_SetRenderDrawBlendMode(pico2d.renderer, SDL_BLENDMODE_BLEND)
