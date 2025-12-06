a720f264e4002736c446cb81ae680acbcd1fd1f2
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-12-06 13:12:04 +0900

    4가지 캐릭터의 프로필 스프라이트 재사용을 위한 모듈 수정

--- al_profile.py
+++ al_profile.py
@@ -1,23 +1,25 @@
 from pico2d import *
 
 class Al_profile:
-    def __init__(self):
+    def __init__(self, x=50, y=720-50, w=100, h=100):
         self.image = load_image("./UI/Al_profile.png")
-        self.x = 50
-        self.y = 720 - 50
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
     def update(self):
         pass
-
     def draw(self):
-        self.image.composite_draw(0, 'h', self.x, self.y, 100, 100)
+        self.image.composite_draw(0, 'h', self.x, self.y, self.w, self.h)
 
 class Al_profile2:
-    def __init__(self):
+    def __init__(self, x=1230, y=720-50, w=100, h=100):
         self.image = load_image("./UI/Al_profile.png")
-        self.x = 1230
-        self.y = 720 - 50
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
     def update(self):
         pass
-
     def draw(self):
-        self.image.draw(self.x, self.y, 100, 100)
+        self.image.draw(self.x, self.y, self.w, self.h)
