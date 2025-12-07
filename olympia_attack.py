8c31a64b81517d829179fd76bf9f6076532b3db7
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-12-08 03:47:48 +0900

    세 번째 캐릭터 attack 충돌처리 구현

--- olympia_attack.py
+++ olympia_attack.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import game_world
 import game_framework
+import Hp_Ui
 
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
 RUN_SPEED_KMPH = 30.0  # Km / Hour
@@ -18,6 +19,7 @@ class Olympia_attack:
         self.face_dir = face_dir
         self.right_end_x = x + 200
         self.left_end_x = x - 200
+        self.hit_objects = set()  # 이미 맞은 객체 추적 (중복 데미지 방지)
 
     def draw(self):
         if Olympia_attack.image and self.face_dir < 0:
@@ -36,6 +38,29 @@ class Olympia_attack:
         elif self.x < 0 or self.x > 1280:
             game_world.remove_object(self)
 
+    def get_bb(self):
+        """충돌 박스 반환"""
+        img = load_image("./Skill_impact/player1_Zizou_Olympia_attack.png")
+        draw_x = self.x
+        half_w = img.w / 2
+        half_h = img.h / 2
+        return draw_x - half_w, self.y - half_h, draw_x + half_w, self.y + half_h
+
+    def handle_collision(self, group, other):
+        if group == 'player1_skill:player2':
+            # 이미 맞은 대상이면 무시
+            if other in self.hit_objects:
+                return
+
+            self.hit_objects.add(other)
+
+            # Player2의 HP 감소
+            for obj in game_world.world[2]:
+                if isinstance(obj, Hp_Ui.Player2_Hp_Ui):
+                    obj.decrease_hp(10)
+                    game_world.remove_object(self)
+                    break
+
 class Olympia_attack2:
     image = None
 
@@ -46,6 +71,7 @@ class Olympia_attack2:
         self.face_dir = face_dir
         self.right_end_x = x + 200
         self.left_end_x = x - 200
+        self.hit_objects = set() # 이미 맞은 객체 추적 (중복 데미지 방지)
 
     def draw(self):
         if Olympia_attack2.image and self.face_dir < 0:
@@ -61,4 +87,27 @@ class Olympia_attack2:
             game_world.remove_object(self)
         elif self.x < 0 or self.x > 1280:
             game_world.remove_object(self)
+
+    def get_bb(self):
+        """충돌 박스 반환"""
+        img = load_image("./Skill_impact/player1_Zizou_Olympia_attack.png")
+        draw_x = self.x
+        half_w = img.w / 2
+        half_h = img.h / 2
+        return draw_x - half_w, self.y - half_h, draw_x + half_w, self.y + half_h
+
+    def handle_collision(self, group, other):
+        if group == 'player2_skill:player1':
+            # 이미 맞은 대상이면 무시
+            if other in self.hit_objects:
+                return
+
+            self.hit_objects.add(other)
+
+            # Player1의 HP 감소
+            for obj in game_world.world[2]:
+                if isinstance(obj, Hp_Ui.Player1_Hp_Ui):
+                    obj.decrease_hp(10)
+                    game_world.remove_object(self)
+                    break
 
