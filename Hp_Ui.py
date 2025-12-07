e8f0f703291dd8d45b13908869a0f82768d6ebb6
SeongMyeongGyo <smksmk798@tukorea.ac.kr>
2025-12-07 20:31:58 +0900

    충돌처리에 따른 Hp 모듈 수정

--- Hp_Ui.py
+++ Hp_Ui.py
@@ -1,23 +1,47 @@
 from pico2d import *
+import game_framework
+
 
 class Player1_Hp_Ui:
-    def __init__(self):
+    def __init__(self, minus_hp=0):
         self.image = load_image("./UI/HP bar.png")
         self.x = 300
         self.y = 720 - 50
+        self.width = 400
+        self.max_width = 400
+        self.height = 30
+        self.time = get_time()
     def update(self):
         pass
 
+    def decrease_hp(self, amount):
+        self.width -= amount
+        if self.width <= 0:
+            self.width = 0
+        self.x = self.width / 2 + 50
+
     def draw(self):
-        self.image.composite_draw(0, 'h', self.x, self.y, 400, 30)
+        self.image.composite_draw(0, 'h', self.x, self.y, self.width, self.height)
+
 
 class Player2_Hp_Ui:
     def __init__(self):
         self.image = load_image("./UI/Hp bar.png")
         self.x = 1280 - 300
         self.y = 720 - 50
+        self.width = 400
+        self.max_width = 400
+        self.height = 30
+        self.time = get_time()
+
     def update(self):
         pass
 
+    def decrease_hp(self, amount):
+        self.width -= amount
+        if self.width <= 0:
+            self.width = 0
+        self.x = 1280 - (self.width / 2 + 50)
+
     def draw(self):
-        self.image.draw(self.x, self.y, 400, 30)
+        self.image.composite_draw(0, 'h', self.x, self.y, self.width, self.height)
